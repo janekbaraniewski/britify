d3b3121990a2e6da5b4f22e2541b88f8f53332ab
Jan Baraniewski <dev@baraniewski.com>
2023-08-11 14:01:59 +0200

    Update

--- src/cli.rs
+++ src/cli.rs
@@ -8,7 +8,7 @@ pub fn run() {
         .arg(
             Arg::with_name("TEXT")
                 .help("Input text to transform")
-                .required(true),
+                .required(false),
         )
         .arg(
             Arg::with_name("style")
@@ -18,6 +18,29 @@ pub fn run() {
                 .takes_value(true),
         )
         .get_matches();
+
+    if matches.value_of("TEXT").is_none() {
+        App::new("Britify")
+            .version("0.1.0")
+            .author("https://baraniew.ski/")
+            .about("Transform yer text intae Scottish, laddie! Britify tak's yer words an' turns 'em intae somethin' pure bonnie. Choose frae slang, formal, or Scottish styles, an' watch as yer text is reborn!")
+            .arg(
+                Arg::with_name("TEXT")
+                    .help("Input text to transform")
+                    .required(false),
+            )
+            .arg(
+                Arg::with_name("style")
+                    .short("s")
+                    .long("style")
+                    .help("Sets the style: slang, formal, scottish")
+                    .takes_value(true),
+            )
+            .print_help()
+            .unwrap();
+        println!(); // Print a newline after the help message
+        return;
+    }
 
     let text = matches.value_of("TEXT").unwrap();
     let style = matches.value_of("style").unwrap_or("slang");
